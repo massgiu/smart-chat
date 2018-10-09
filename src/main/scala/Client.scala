940cbf1b169aef1a19853a977b410cdd0f77c781
Massimiliano Giunchi <massimiliano.giunchi@studio.unibo.it>
2018-10-09 11:56:56 +0200

    Modified name object Client

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -1,3 +1,15 @@
 class Client {
 
 }
+
+object Client{
+
+  //Message from server
+  final case class AcceptRegistrationFromRegister(accept: Boolean)
+
+  final case class UserAndGroupActive(userList: List[String],groupList : List[String])
+
+  final case class StringMessage(message : String)
+
+  final case class AttachmentMessage()
+}
