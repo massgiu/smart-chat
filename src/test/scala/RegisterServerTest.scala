a3155030fc00531b4912a658cbf19caf5c336a71
giacomo <giacomo.scaparrotti@studio.unibo.it>
2018-10-12 15:46:42 +0200

    fixed some issues in test class

--- src/test/scala/RegisterServerTest.scala
+++ src/test/scala/RegisterServerTest.scala
@@ -12,18 +12,21 @@ class RegisterServerTest extends TestKit(ActorSystem("MySpec")) with ImplicitSen
 
   "A Register Server" must {
     "send an answer to a client asking to join it" in {
-      val server = system.actorOf(Props[RegisterServer], name = "welcomeServer")
-      server.tell(RegisterServer.JoinRequest, this.testActor)
+      val server = system.actorOf(Props[RegisterServer], name = "welcomeServer1")
+      server.tell(RegisterServer.JoinRequest("ciao"), this.testActor)
       expectMsgClass(classOf[Client.AcceptRegistrationFromRegister])
     }
     "send a list of all users when a client asks it" in {
-      val server = system.actorOf(Props[RegisterServer], name = "welcomeServer")
+      val server = system.actorOf(Props[RegisterServer], name = "welcomeServer2")
       server.tell(RegisterServer.AllUsersAndGroupsRequest, this.testActor)
       expectMsgClass(classOf[UserAndGroupActive])
     }
     "Respond to a client when it wants to create a new one-to-one chat" in {
-      val server = system.actorOf(Props[RegisterServer], name = "welcomeServer")
-      server.tell(RegisterServer.NewOneToOneChatRequest, this.testActor)
+      val clientName = "aClient"
+      val server = system.actorOf(Props[RegisterServer], name = "welcomeServer3")
+      server.tell(RegisterServer.JoinRequest(clientName), this.testActor)
+      expectMsgClass(classOf[Client.AcceptRegistrationFromRegister])
+      server.tell(RegisterServer.NewOneToOneChatRequest(clientName), this.testActor)
       expectMsgClass(classOf[ResponseForChatCreation])
     }
   }
