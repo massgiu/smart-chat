20ccc3e5676f6d085b5d3034cf433d3f6b8d55f5
giacomo <giacomo.scaparrotti@studio.unibo.it>
2018-10-15 22:30:11 +0200

    implemented part of the receive method in the register server according to its tests

--- src/main/scala/RegisterServer.scala
+++ src/main/scala/RegisterServer.scala
@@ -1,13 +1,37 @@
-import akka.actor.Actor
+import Client.{AcceptRegistrationFromRegister, ResponseForChatCreation, UserAndGroupActive}
+import RegisterServer._
+import akka.actor.{Actor, ActorRef}
 
 class RegisterServer extends Actor{
-  override def receive: Receive = ???
+
+  var users: Map[String, ActorRef] = Map.empty
+  var groups: Map[String, List[String]] = Map.empty
+
+  override def receive: Receive = {
+    case JoinRequest(clientName) =>
+      users.find(_._1 == clientName).fold({
+        users += (clientName -> sender)
+        sender ! AcceptRegistrationFromRegister(true)
+      })(_ => sender ! AcceptRegistrationFromRegister(false))
+    case NewGroupChatRequest(newGroupName) =>
+      groups.find(_._1 == newGroupName).fold({
+        users += (newGroupName -> List.empty)
+        sender ! AcceptRegistrationFromRegister(true)
+      })(_ => sender ! AcceptRegistrationFromRegister(false))
+    case AllUsersAndGroupsRequest =>
+      sender ! UserAndGroupActive(users.keys.toList, groups.keys.toList)
+    case NewOneToOneChatRequest(friendName) =>
+      //fold(if_not_present)(if_present)
+      users.find(_._1 == friendName).fold(sender ! ResponseForChatCreation(false))(_ => sender ! ResponseForChatCreation(true))
+    case JoinGroupChatRequest(group) =>
+      groups.find(_._1 == group).fold(sender ! ResponseForChatCreation(false))(_ => sender ! ResponseForChatCreation(true))
+  }
 }
 
 object RegisterServer {
   case class JoinRequest(clientName:String)
   case class AllUsersAndGroupsRequest()
   case class NewOneToOneChatRequest(friendName:String)
-  case class NewGroupChatRequest()
+  case class NewGroupChatRequest(newGroupName:String)
   case class JoinGroupChatRequest(group:String)
 }
