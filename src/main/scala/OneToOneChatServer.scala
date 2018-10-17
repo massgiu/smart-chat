3aa3e4c8bf2717c71cce085a787d831f401ae115
giacomo <giacomo.scaparrotti@studio.unibo.it>
2018-10-17 10:24:07 +0200

    implemented onetoonechatserver and modified registerserver consequently

--- src/main/scala/OneToOneChatServer.scala
+++ src/main/scala/OneToOneChatServer.scala
@@ -1,4 +1,18 @@
-class OneToOneChatServer {
+import OneToOneChatServer.Message
+import akka.actor.{Actor, ActorRef}
+
+class OneToOneChatServer(one: ActorRef, two: ActorRef) extends Actor {
+
+  val memberOne: ActorRef = one
+  val memberTwo: ActorRef = two
+
+  override def receive: Receive = {
+    case Message(text) => sender() match {
+      case `memberOne` => memberTwo ! Message(text)
+      case `memberTwo` => memberOne ! Message(text)
+    }
+    case _ => println("unknown message")
+  }
 
 }
 
