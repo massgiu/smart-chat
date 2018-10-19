e198fd254b3a86a10e24068aec28a747bf3d3b15
Massimiliano Giunchi <massimiliano.giunchi@studio.unibo.it>
2018-10-19 17:04:20 +0200

    Added to ChatInteractionTest test for chat one to one between two clients

--- src/test/scala/ChatInteractionTest.scala
+++ src/test/scala/ChatInteractionTest.scala
@@ -1,4 +1,5 @@
-import Client.{AcceptRegistrationFromRegister, ResponseForChatCreation, UserAndGroupActive}
+import Client.{AcceptRegistrationFromRegister, ResponseForChatCreation, StringMessageFromServer, UserAndGroupActive}
+import OneToOneChatServer.Message
 import RegisterServer.JoinRequest
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
@@ -32,11 +33,29 @@ class ChatInteractionTest extends TestKit(ActorSystem("MySpec")) with ImplicitSe
       clientTwo.expectMsg(UserAndGroupActive(List("clientOne","clientTwo"),List()))
 
       //Request from ClientOne to create a oneToOne chat with ClientTwo
-      clientOne.send(server,RegisterServer.NewOneToOneChatRequest("clientOne"))
-      clientOne.expectMsgPF()({
-        case ResponseForChatCreation(true, actor) if actor.isDefined => Unit
+      clientOne.send(server,RegisterServer.NewOneToOneChatRequest("clientTwo"))
+
+      val testchatServer = clientOne.expectMsgPF()({
+        case ResponseForChatCreation(true, oneToOneServer) if oneToOneServer.isDefined => oneToOneServer.get
       })
 
+      //Start chat bewtween two users
+
+      clientOne.send(testchatServer,Message("msgFromClientOne"))
+      clientTwo.expectMsgPF()({
+        case StringMessageFromServer("msgFromClientOne", messageNumber) if messageNumber.isValidLong => Unit
+      })
+      clientOne.expectMsgPF()({
+        case StringMessageFromServer("msgFromClientOne", messageNumber) if messageNumber.isValidLong => Unit
+      })
+
+      clientTwo.send(testchatServer,Message("msgFromClientTwo"))
+      clientOne.expectMsgPF()({
+        case StringMessageFromServer("msgFromClientTwo", messageNumber) if messageNumber.isValidLong => Unit
+      })
+      clientTwo.expectMsgPF()({
+        case StringMessageFromServer("msgFromClientTwo", messageNumber) if messageNumber.isValidLong => Unit
+      })
     }
 
   }
