f41f99429e356b24d1bd8d5ea447be23ea522d54
giacomo <giacomo.scaparrotti@studio.unibo.it>
2018-10-19 17:13:29 +0200

    first implementation of groupchatserver

---
+++ src/main/scala/GroupChatServer.scala
@@ -0,0 +1,26 @@
+import Client.StringMessageFromServer
+import GroupChatServer.{AddMember, GroupMessage, RemoveMember}
+import akka.actor.{Actor, ActorRef}
+
+class GroupChatServer(m: Set[ActorRef] = Set.empty) extends Actor {
+
+  var members: Set[ActorRef] = m
+  private var messageNumber: Long = 0
+
+  override def receive: Receive =  {
+    case AddMember(member) => members += member
+    case RemoveMember(member) => members -= member
+    case GroupMessage(text) =>
+      messageNumber += 1
+      members.foreach(m => m ! StringMessageFromServer(text, messageNumber))
+    case _ => println("unknown message")
+  }
+
+}
+
+object GroupChatServer {
+  case class AddMember(member: ActorRef)
+  case class RemoveMember(member: ActorRef)
+  case class GroupMessage(text: String)
+  case class GroupAttachment(payload:AttachmentContent)
+}
