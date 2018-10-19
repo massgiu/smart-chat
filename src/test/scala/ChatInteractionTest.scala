72eead290a8b3bdf3178a1b94d85f48ec7eb7537
Massimiliano Giunchi <massimiliano.giunchi@studio.unibo.it>
2018-10-19 16:08:50 +0200

    Added functionality to ChatInteractionTest

--- src/test/scala/ChatInteractionTest.scala
+++ src/test/scala/ChatInteractionTest.scala
@@ -1,6 +1,6 @@
-import Client.{AcceptRegistrationFromRegister, UserAndGroupActive}
+import Client.{AcceptRegistrationFromRegister, ResponseForChatCreation, UserAndGroupActive}
 import RegisterServer.JoinRequest
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
@@ -20,16 +20,22 @@ class ChatInteractionTest extends TestKit(ActorSystem("MySpec")) with ImplicitSe
       val server = system.actorOf(Props[RegisterServer], name = "welcomeServer")
 
       //ClientOne registration
-      clientOne.send(server, RegisterServer.JoinRequest(clientOne.toString))
-      clientOne.expectMsgClass(AcceptRegistrationFromRegister(true).getClass)
+      clientOne.send(server, RegisterServer.JoinRequest("clientOne"))
+      clientOne.expectMsg(AcceptRegistrationFromRegister(true))
       clientOne.send(server,RegisterServer.AllUsersAndGroupsRequest)
-      clientOne.expectMsgClass(UserAndGroupActive(List("clientOne"),List()).getClass)
+      clientOne.expectMsg(UserAndGroupActive(List("clientOne"),List()))
       //ClientTwo registration
-      clientTwo.send(server, RegisterServer.JoinRequest(clientTwo.toString))
-      clientTwo.expectMsgClass(AcceptRegistrationFromRegister(true).getClass)
+
+      clientTwo.send(server, RegisterServer.JoinRequest("clientTwo"))
+      clientTwo.expectMsg(AcceptRegistrationFromRegister(true))
       clientTwo.send(server,RegisterServer.AllUsersAndGroupsRequest)
+      clientTwo.expectMsg(UserAndGroupActive(List("clientOne","clientTwo"),List()))
+
       //Request from ClientOne to create a oneToOne chat with ClientTwo
-      clientOne.send(server,RegisterServer.NewOneToOneChatRequest(clientOne.toString))
+      clientOne.send(server,RegisterServer.NewOneToOneChatRequest("clientOne"))
+      clientOne.expectMsgPF()({
+        case ResponseForChatCreation(false, actor) if actor.isDefined => Unit
+      })
 
     }
 
