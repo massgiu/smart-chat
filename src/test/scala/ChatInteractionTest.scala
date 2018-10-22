204763d6f937076c11492cd8252a6dd7aecfda46
Massimiliano Giunchi <massimiliano.giunchi@studio.unibo.it>
2018-10-22 17:04:38 +0200

    Separated tests and added descriptions in ChatInteractionTest class

--- src/test/scala/ChatInteractionTest.scala
+++ src/test/scala/ChatInteractionTest.scala
@@ -18,27 +18,31 @@ class ChatInteractionTest extends TestKit(ActorSystem("MySpec")) with ImplicitSe
 
   "A right interaction between client and server" must {
 
-    "accept a new client asking to join it and add it to the register" in {
+    val clientOne = TestProbe("clientOne")
+    val clientTwo = TestProbe("clientTwo")
+    val clientWithoutUserName = TestProbe("clientWithoutUserName")
+    val server = system.actorOf(Props[RegisterServer], name = "welcomeServer")
 
-      val clientOne = TestProbe("clientOne")
-      val clientTwo = TestProbe("clientTwo")
-      val clientWithoutUserName = TestProbe("clientWithoutUserName")
-      val server = system.actorOf(Props[RegisterServer], name = "welcomeServer")
+    "accept clients request to register themselves and give back users list and chat group list" in {
 
       //ClientOne registration with username
       clientOne.send(server, RegisterServer.JoinRequest("clientOne"))
       clientOne.expectMsg(AcceptRegistrationFromRegister(true))
-      clientOne.send(server,RegisterServer.AllUsersAndGroupsRequest)
-      clientOne.expectMsg(UserAndGroupActive(List("clientOne"),List()))
+      clientOne.send(server, RegisterServer.AllUsersAndGroupsRequest)
+      clientOne.expectMsg(UserAndGroupActive(List("clientOne"), List()))
 
       //ClientTwo registration with username
       clientTwo.send(server, RegisterServer.JoinRequest("clientTwo"))
       clientTwo.expectMsg(AcceptRegistrationFromRegister(true))
-      clientTwo.send(server,RegisterServer.AllUsersAndGroupsRequest)
-      clientTwo.expectMsg(UserAndGroupActive(List("clientOne","clientTwo"),List()))
+      clientTwo.send(server, RegisterServer.AllUsersAndGroupsRequest)
+      clientTwo.expectMsg(UserAndGroupActive(List("clientOne", "clientTwo"), List()))
+    }
+
+    "accept a client request to create an oneToOneChat, accept the request of a second Client to join to it" +
+      " and then exchange messages between them" in {
 
       //Request from ClientOne to create a oneToOne chat with ClientTwo
-      clientOne.send(server,RegisterServer.NewOneToOneChatRequest("clientTwo"))
+      clientOne.send(server, RegisterServer.NewOneToOneChatRequest("clientTwo"))
 
       val testchatServer = clientOne.expectMsgPF()({
         case ResponseForChatCreation(true, oneToOneServer) if oneToOneServer.isDefined => oneToOneServer.get
@@ -46,7 +50,7 @@ class ChatInteractionTest extends TestKit(ActorSystem("MySpec")) with ImplicitSe
 
       //Start chat bewtween two users
       val firstMessageText = "msgFromClientOne"
-      clientOne.send(testchatServer,Message(firstMessageText))
+      clientOne.send(testchatServer, Message(firstMessageText))
       val firstMessageIndex = clientTwo.expectMsgPF()({
         case StringMessageFromServer(`firstMessageText`, messageNumber) => messageNumber
       })
@@ -56,47 +60,66 @@ class ChatInteractionTest extends TestKit(ActorSystem("MySpec")) with ImplicitSe
 
       val secondMessageText = "msgFromClientTwo"
       val secondMessageIndex = firstMessageIndex + 1
-      clientTwo.send(testchatServer,Message(secondMessageText))
+      clientTwo.send(testchatServer, Message(secondMessageText))
       clientOne.expectMsgPF()({
         case StringMessageFromServer(`secondMessageText`, `secondMessageIndex`) => Unit
       })
       clientTwo.expectMsgPF()({
         case StringMessageFromServer(`secondMessageText`, `secondMessageIndex`) => Unit
       })
+    }
+
+    "accept a client request to create a chat group and a second client request to join to it" in {
 
       //Request from ClientOne to create a chatGroup
       clientOne.send(server, RegisterServer.NewGroupChatRequest("chatGroupName"))
       clientOne.expectMsg(AcceptRegistrationFromRegister(true))
 
-      //Request from ClientOne to join to chatGroup name "chatGroupName"
+      //Request from ClientOne to join to chatGroup named "chatGroupName"
       clientOne.send(server, RegisterServer.JoinGroupChatRequest("chatGroupName"))
       clientOne.expectMsgPF()({
         case ResponseForChatCreation(true, chatGroupServer) => Unit
       })
 
-      //Request from ClientTwo to join to chatGroup name "chatGroupName"
+      //Request from ClientTwo to join to chatGroup named "chatGroupName"
       clientTwo.send(server, RegisterServer.JoinGroupChatRequest("chatGroupName"))
-        clientTwo.expectMsgPF()({
+      clientTwo.expectMsgPF()({
         case ResponseForChatCreation(true, chatGroupServer) => Unit
       })
+    }
+
+    "refuse a client request, without user name, to register to server" in {
 
       //clientWithoutUserName registration
-      clientWithoutUserName.send(server,RegisterServer.JoinRequest(null))
+      clientWithoutUserName.send(server, RegisterServer.JoinRequest(null))
       clientWithoutUserName.expectMsg(AcceptRegistrationFromRegister(true))
+    }
+
+    "refuse a client request with a username already used, to register to server" in {
 
       //ClientOne registration with same username
       clientOne.send(server, RegisterServer.JoinRequest("clientOne"))
       clientOne.expectMsg(AcceptRegistrationFromRegister(false))
 
+    }
+
+    "refuse a client request to create an oneToOne chat with an inexisting user" in {
+
       //ClientOne create a oneToOnechatGroup with an inexistent user
-      clientOne.send(server,RegisterServer.NewOneToOneChatRequest("inexistentClient"))
+      clientOne.send(server, RegisterServer.NewOneToOneChatRequest("inexistentClient"))
       clientOne.expectMsgPF()({
         case ResponseForChatCreation(false, oneToOneServer) => Unit
       })
+    }
+
+    "refuse a client request to create a chat group chat with a name already used" in {
 
       //ClientOne create a chatGroup with same group name
       clientOne.send(server, RegisterServer.NewGroupChatRequest("chatGroupName"))
       clientOne.expectMsg(AcceptRegistrationFromRegister(false))
+    }
+
+    "refuse a client request to join to a chat group chat with an inexisting " in {
 
       //ClientOne request to join to an inexistent chat group
       clientOne.send(server, RegisterServer.JoinGroupChatRequest("inexistentChatGroup"))
