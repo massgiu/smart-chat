2c1ee286079c2fb22e439be6b3f6a23b2e12d80d
Massimiliano Giunchi <massimiliano.giunchi@studio.unibo.it>
2018-11-01 11:16:24 +0100

    Removed "system" parameter from LaunchRegisterServer

--- src/main/scala/LaunchRegisterServer.scala
+++ src/main/scala/LaunchRegisterServer.scala
@@ -6,5 +6,5 @@ import com.typesafe.config.ConfigFactory
 object LaunchRegisterServer extends App {
   val config = ConfigFactory.parseFile(new File("src/main/scala/res/server.conf"))
   val system = ActorSystem.create("MySystem", config)
-  system.actorOf(Props(new RegisterServer(system)), name = "server")
+  system.actorOf(Props(new RegisterServer()), name = "server")
 }
